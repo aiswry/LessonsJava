e306fc9f833a58be634448f4d5b2276d5efecbd5
aisdhar <75646664+aisdhar@users.noreply.github.com>
2021-11-12 23:42:43 -0500

    2 commit forloop

---
+++ JavaLessons/src/EnhancedFor.java
@@ -0,0 +1,23 @@
+
+public class EnhancedFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int a[]= new int[5];
+		int v=1;
+		for(int i=1;i<5;i++) {
+			a[i]= v++;
+			System.out.println("a[i] values are    "   +  a[i]);
+		}
+		
+		
+		for(int k:a)   //for each iteration value of a goes to k and is printed
+		//on 1st iteration 1st value of a to k and is printed.and then on 2nd iteration second value
+		{
+			System.out.println(k);
+
+		}
+	}
+
+}
